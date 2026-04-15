564c9d066a30c49d257e152478ab621a37d5845d
enrikoyr <enrikogreen@gmail.com>
2026-04-15 14:19:55 +0700

    testing PIN implementation

--- schema.sql
+++ schema.sql
@@ -1,6 +1,12 @@
-CREATE TABLE IF NOT EXISTS students (
+DROP TABLE IF EXISTS students;
+
+CREATE TABLE students (
     id TEXT PRIMARY KEY,
     name TEXT NOT NULL,
+    pin TEXT NOT NULL,
     proposalDate TEXT NOT NULL,
-    createdAt TEXT DEFAULT CURRENT_TIMESTAMP
+    createdAt TEXT DEFAULT CURRENT_TIMESTAMP,
+    bab3CompletedAt TEXT,
+    bab4CompletedAt TEXT,
+    bab5CompletedAt TEXT
 );
